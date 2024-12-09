a855c2a4d5c50023d08e52ae4a287f958bb83f67
Woncheol Yang <sundancekid1108@gmail.com>
2024-12-09 15:24:33 +0900

    eslint추가
    eslint추가

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -4,13 +4,13 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  {
-    rules: {
-      "no-console": "error",
-      "no-unused-vars": "error",
-      "no-undef": "error"
-    }
-  },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            "no-console": "error",
+            "no-unused-vars": "error",
+            "no-undef": "error",
+        },
+    },
 ];
