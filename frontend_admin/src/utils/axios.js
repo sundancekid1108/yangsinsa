b89dd08575f3312a38ec56622265fd3066a26348
Woncheol Yang <sundancekid1108@gmail.com>
2025-02-13 11:14:21 +0900

    axios 인터셉터 작업
    accessToken  만료시 RefreshToken을 활용하여 accessToken 재발행후 교체,
    accessToken, RefreshToken만료시 로그아웃처리

--- frontend_admin/src/utils/axios.js
+++ frontend_admin/src/utils/axios.js
@@ -1,10 +1,63 @@
-import axios from 'axios'
+import axios from 'axios';
+import { Outlet, useLocation, useNavigate, redirect } from 'react-router-dom';
 
-const BASE_URL = process.env.REACT_APP_BASE_URL
-
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL
-})
+	baseURL: BASE_URL,
+	withCredentials: true,
+	timeout: 5000,
+});
 
-export default axiosInstance
+//요청시 처리
+axiosInstance.interceptors.request.use(
+	(config) => {
+		// console.log(config.url);
+		return config;
+	},
+	(error) => {
+		// console.log('인터셉터 req ', error);
+		return Promise.reject(error);
+	},
+);
+
+//응답시 처리
+axiosInstance.interceptors.response.use(
+	// 200번대 응답이 올때 처리
+	(response) => {
+		console.log('인스탄스 성공!!');
+		return response;
+	},
+
+	async (error) => {
+		// 401, 403, 404, 500 에러일 경우
+		console.log('인스탄스 실패!!');
+		// console.log(error);
+		const originalRequest = error.config;
+
+		if (error.response && error.response.status === 401) {
+			try {
+				const response = await axiosInstance.get(
+					'/admin/auth/updateaccessetoken',
+				);
+				console.log(response);
+				const headers = response.headers;
+				const newAccessToken = headers.authorization;
+				console.log(newAccessToken);
+				localStorage.setItem('token', newAccessToken);
+				originalRequest.headers.authorization = newAccessToken;
+				return axiosInstance(originalRequest);
+			} catch (error) {
+				return Promise.reject(error);
+			}
+		}
+
+		if (error.response && error.response.status === 403) {
+			console.log('인터셉터 테스트, 토큰 만료');
+			localStorage.removeItem('token');
+			return Promise.reject(error);
+		}
+	},
+);
+
+export default axiosInstance;
